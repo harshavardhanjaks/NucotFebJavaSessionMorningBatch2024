dc86b841905fd76758d7904714279f2a487dde21
HarshaVardhanjaks <jakkampudi@gmail.com>
2024-02-23 15:31:28 +0530

    Add existing file

---
+++ MathematicalDemo.java
@@ -0,0 +1,123 @@
+package javasessions.com;
+
+public class MathematicalDemo {
+	public static void main(String[] args) {
+		
+			
+		int a=10;
+		
+		int b=30;
+		
+		System.out.println(a+b);
+		
+		System.out.println(a-b);
+		
+		System.out.println(a*b);
+		
+		System.out.println(a=b);
+		
+		System.out.println(a%b);
+		
+		System.out.println(a%b);
+		
+	//**************************************************	
+		
+		
+		
+		System.out.println("*");
+		
+		String s="Welcome nucot";
+		
+		System.out.println(s);
+		
+		String ss="Bangalore";
+		
+		System.out.println(ss);
+		
+		System.out.println(s+ss);
+		
+		System.out.println(a+s);
+		
+		System.out.println(a+b+s);
+		
+		System.out.println(a+b+s+ss);
+		
+		System.out.println(s+ss+a+b);
+		
+		System.out.println(s+ss+(a+b));
+		
+		System.out.println("*");	
+		
+		char c='a';
+		
+		char cc='b';
+		
+		System.out.println(c+ss);
+		
+		System.out.println(c+s);
+		
+		System.out.println(c+cc+s);
+		
+		System.out.println(c+cc+s+ss);
+		
+		System.out.println(s+ss+c+cc);
+		
+		System.out.println(s+ss+(c+cc));
+		
+	    System.out.println("*");
+	    
+		short sh=14;
+		
+		short shh=10;
+		
+		System.out.println(s+ss);
+		
+		System.out.println(sh+s);
+		
+		System.out.println(sh+shh+s);
+		
+		System.out.println(sh+shh+s+ss);
+		
+		System.out.println(s+ss+sh+shh);
+		
+		System.out.println(s+ss+(sh+shh));
+		
+		System.out.println("*");
+		
+		float f=10.00f;
+		
+		float ff=1.40239846e-45f;
+		
+		System.out.println(f+ff);
+		
+		System.out.println(f+s+sh);
+		
+		System.out.println(f+ff+(a+b));
+		
+		System.out.println("*");
+		
+	    double d=22.000;
+		 
+        double dd=60.000;
+         
+        System.out.println(d+dd);
+         
+        System.out.println(d+ss);
+         
+        System.out.println(d+dd+s);
+         
+        System.out.println(d+dd+s+ss);
+         
+        System.out.println(s+ss+d+dd);
+         
+        System.out.println(s+ss+(a+b)); 
+         
+        System.out.println("*");
+         
+        
+	
+	
+		
+		 
+	}
+}
