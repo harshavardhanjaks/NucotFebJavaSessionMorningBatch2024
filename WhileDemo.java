87fa240572107f8d71e10809f67a39ed049ca57b
HarshaVardhanjaks <jakkampudi@gmail.com>
2024-03-02 18:39:09 +0530

    WhileDemo.java

---
+++ WhileDemo.java
@@ -0,0 +1,20 @@
+package javasessions.com;
+
+public class WhileDemo 
+{
+
+
+public static void main(String[] args) {
+	System.out.println("Numbers from 1 to to 10");
+	
+	int i=0;
+	while(i<=5)
+	{
+		System.out.println(i);
+		i++;
+	}
+	
+	System.out.println("Bye");
+	
+}
+}
