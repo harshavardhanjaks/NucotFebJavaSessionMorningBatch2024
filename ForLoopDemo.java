ff7850e7742a202a792e49f31a45f3960d38b906
HarshaVardhanjaks <jakkampudi@gmail.com>
2024-03-04 18:00:33 +0530

    LoopDemo

---
+++ ForLoopDemo.java
@@ -0,0 +1,83 @@
+package javasessions.com;
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		
+			
+			//for loop
+			/*
+			 * 
+			 * 
+			 * for(variable_init;variable_condition;variable_updation(++/--))
+			 * {
+			 * 
+			 * 		body or statements
+			 * 
+			 * }
+			 * 
+			 * 
+			 * 
+			 */
+			   System.out.println("Numbers from 1 to 10");
+				
+
+				for(int i=0;i<=10;i++)
+				{
+					
+					System.out.println(i);
+					
+				}
+				
+				
+			System.out.println("thanks");	
+				
+			System.out.println("Numbers from 2 to 10");
+			
+
+			for(int ii=2;ii<=10;ii=ii+2)
+			{
+				
+				System.out.println(ii);
+				
+			}
+			
+			
+		System.out.println("thanks");	
+			
+				
+				System.out.println("Numbers from 1 to 10");
+				
+				
+				for(int iii=1;iii<=10;iii=iii+2)
+				{
+					
+					System.out.println(iii);
+					
+				}
+				
+				
+				System.out.println("thanks");	
+                    
+				/*System.out.println("Numbers from 1 to 10");
+				
+
+				for(int i=0;i<=10;i--)
+				{
+					
+					System.out.println(i);
+					
+				}
+				
+				
+			System.out.println("thanks");	*/
+				
+				
+				
+
+			}
+
+		
+	
+
+}
---
+++ LoopsDemo.java
@@ -0,0 +1,90 @@
+package javasessions.com;
+
+public class LoopsDemo {
+
+	public static void main(String[] args) {
+		
+			
+			
+			//While loop
+			
+			
+			
+			
+				
+				
+				
+				//while loop--> minimum number of times while will execute -->0
+				/*
+				 * syntax
+				 * 
+				 * while(condition)
+				 * {
+				 * 
+				 * 
+				 * 
+				 * 
+				 * }
+				 * 
+				 * 
+				 */
+				System.out.println("Numbers from 1 to 10");
+				
+				int i=1;
+				
+				while(i<=100)
+				{
+					System.out.println(i);//1,2,3,4 5-----10
+					i++;//i+1=2 3 4 5 6----
+					
+				}
+				
+				System.out.println("Thanks");
+				
+				
+				
+				
+				System.out.println("Even numbers from 2 to 10");
+				
+				int ii=2;
+				while(ii<=10)
+				{
+					System.out.println(ii);//2 4 6 8 10
+					ii=ii+2;
+				}
+				
+				
+		System.out.println("oddnumbers from 2 to 10");
+				
+				int iii=1;
+				while(iii<=10)
+				{
+					System.out.println(iii);//1 3 5 7 9
+					iii=iii+2;
+				}
+				
+				System.out.println("thanks");
+				
+				
+
+				//infinite while  loop 
+				
+				/*while(true)
+					
+				{
+					System.out.println("Welcome to nucot");
+					
+				}*/
+				
+				
+				
+				
+				
+
+			}
+
+		
+
+	
+
+}
